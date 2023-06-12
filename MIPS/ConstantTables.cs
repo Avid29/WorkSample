1f27d0f22ca0414cd45a6d90379513df81ab1614
Adam Dernis <Avid29@Live.com>
2023-06-12 12:47:09 -0400

    Add project files.

---
+++ MIPS/ConstantTables.cs
@@ -0,0 +1,163 @@
+// Adam Dernis 2023
+
+
+namespace WorkSample;
+
+/// <summary>
+/// A class containing constant tables for parsing lookup.
+/// </summary>
+public static class ConstantTables
+{
+    // R type patterns
+    private static readonly Argument[] StandardRPattern = {Argument.RD, Argument.RS, Argument.RT};                      // <instr>  $rd, $rs, $rt
+    private static readonly Argument[] MultiplyRPattern = {Argument.RS, Argument.RT};                                   // <instr>  $rs, $rt
+    private static readonly Argument[] ShiftPattern = {Argument.RD, Argument.RT, Argument.Shift};                       // <instr>  $rd, $rt, sa
+    private static readonly Argument[] VariableShiftPattern = {Argument.RD, Argument.RT, Argument.RS};                  // <instr>  $rd, $rt, $rs
+    private static readonly Argument[] JumpRegisterPattern = {Argument.RS};                                             // <instr>  $rs
+    private static readonly Argument[] MoveFromPattern = {Argument.RD};                                                 // <instr>  $rd
+    private static readonly Argument[] MoveToPattern = {Argument.RS};                                                   // <instr>  $rs
+    
+    // I type patterns
+    private static readonly Argument[] StandardIPattern = {Argument.RT, Argument.RS, Argument.Immediate};               // <instr>  $rt, $rs, imm
+    private static readonly Argument[] BranchComparePattern = {Argument.RS, Argument.RT, Argument.Immediate};           // <instr>  $rs, $rt, offset
+    private static readonly Argument[] BranchPattern = {Argument.RS,  Argument.Immediate};                              // <instr>  $rs, offset
+    private static readonly Argument[] LoadImmediatePattern = {Argument.RT, Argument.Immediate};                        // <instr>  $rt, imm
+    private static readonly Argument[] MemoryPattern = {Argument.RT, Argument.AddressOffset};                           // <instr>  $rt, offset($rs)
+
+    // J type patterns
+    private static readonly Argument[] JumpPattern = {Argument.Address};                                                // <instr>  addr
+
+    private static readonly Dictionary<string, InstructionMetadata> _instructionTable = new()
+    {
+        { "sll", new InstructionMetadata("sll", FunctionCode.ShiftLeftLogical, ShiftPattern) },                         // sll      $rd, $rt, sa
+        { "srl", new InstructionMetadata("srl", FunctionCode.ShiftRightLogical, ShiftPattern) },                        // ssl      $rd, $rt, sa
+        { "sra", new InstructionMetadata("sra", FunctionCode.ShiftRightArithmetic, ShiftPattern) },                     // sra      $rd, $rt, sa
+
+        { "sllv", new InstructionMetadata("sllv", FunctionCode.ShiftLeftLogicalVariable, VariableShiftPattern) },       // sllv     $rd, $rt, $rs
+        { "srlv", new InstructionMetadata("srlv", FunctionCode.ShiftRightLogicalVariable, VariableShiftPattern) },      // srlv     $rd, $rt, $rs
+        { "srav", new InstructionMetadata("srav", FunctionCode.ShiftRightArithmeticVariable, VariableShiftPattern) },   // srav     $rd, $rt, $rs
+
+        { "jr", new InstructionMetadata("jr", FunctionCode.JumpRegister, JumpRegisterPattern) },                        // jr       $rs
+        { "jalr", new InstructionMetadata("jalr", FunctionCode.JumpAndLinkRegister, JumpRegisterPattern) },             // jalr     $rs
+
+        { "syscall", new InstructionMetadata("syscall", FunctionCode.SystemCall, Array.Empty<Argument>()) },            // syscall
+
+        { "mfhi", new InstructionMetadata("mfhi", FunctionCode.MoveFromHigh, MoveFromPattern) },                        // mfhi     $rd
+        { "mthi", new InstructionMetadata("mthi", FunctionCode.MoveToHigh, MoveToPattern) },                            // mthi     $rs
+        { "mflo", new InstructionMetadata("mflo", FunctionCode.MoveFromLow, MoveFromPattern) },                         // mflo     $rd
+        { "mtlo", new InstructionMetadata("mtlo", FunctionCode.MoveToLow, MoveToPattern) },                             // mtlo     $rs
+
+        { "mult", new InstructionMetadata("mult", FunctionCode.Multiply, MultiplyRPattern) },                           // mult     $rs, $rt
+        { "multu", new InstructionMetadata("multu", FunctionCode.MultiplyUnsigned, MultiplyRPattern) },                 // multu    $rs, $rt
+        { "div", new InstructionMetadata("div", FunctionCode.Divide, MultiplyRPattern) },                               // div      $rs, $rt
+        { "divu", new InstructionMetadata("divu", FunctionCode.DivideUnsigned, MultiplyRPattern) },                     // divu     $rs, $rt
+
+        { "add", new InstructionMetadata("add", FunctionCode.Add, StandardRPattern) },                                  // add      $rd, $rs, $rt
+        { "addu", new InstructionMetadata("addu", FunctionCode.AddUnsigned, StandardRPattern) },                        // addu     $rd, $rs, $rt
+        { "sub", new InstructionMetadata("sub", FunctionCode.Subtract, StandardRPattern) },                             // sub      $rd, $rs, $rt
+        { "subu", new InstructionMetadata("subu", FunctionCode.SubtractUnsigned, StandardRPattern) },                   // subu     $rd, $rs, $rt
+
+        { "and", new InstructionMetadata("and", FunctionCode.And, StandardRPattern) },                                  // and      $rd, $rs, $rt
+        { "or", new InstructionMetadata("or", FunctionCode.Or, StandardRPattern) },                                     // or       $rd, $rs, $rt
+        { "xor", new InstructionMetadata("xor", FunctionCode.ExclusiveOr, StandardRPattern) },                          // xor      $rd, $rs, $rt
+        { "nor", new InstructionMetadata("nor", FunctionCode.Nor, StandardRPattern) },                                  // nor      $rd, $rs, $rt
+
+        { "slt", new InstructionMetadata("slt", FunctionCode.SetLessThan, StandardRPattern) },                          // slt      $rd, $rs, $rt
+        { "sltu", new InstructionMetadata("sltu", FunctionCode.SetLessThanUnsigned, StandardRPattern) },                // sltu     $rd, $rs, $rt
+        
+        { "j", new InstructionMetadata("j", OperationCode.Jump, JumpPattern) },                                         // j        addr
+        { "jal", new InstructionMetadata("jal", OperationCode.JumpAndLink, JumpPattern) },                              // jal      addr
+
+        { "beq", new InstructionMetadata("beq", OperationCode.BranchOnEquals, BranchComparePattern) },                  // beq      $rs, $rt, offset
+        { "bne", new InstructionMetadata("bne", OperationCode.BranchOnNotEquals, BranchComparePattern) },               // bne      $rs, $rt, offset
+        { "blez", new InstructionMetadata("blez", OperationCode.BranchOnLessThanOrEqualToZero, BranchPattern) },        // blez     $rs, offset
+        { "bgtz", new InstructionMetadata("bgtz", OperationCode.BranchGreaterThanZero, BranchPattern) },                // bgtz     $rs, offset
+
+        { "addi", new InstructionMetadata("addi", OperationCode.AddImmediate, StandardIPattern) },                      // addi     $rt, $rs, imm
+        { "addiu", new InstructionMetadata("addiu", OperationCode.AddImmediateUnsigned, StandardIPattern) },            // addiu    $rt, $rs, imm
+
+        { "slti", new InstructionMetadata("slti", OperationCode.SetLessThanImmediate, StandardIPattern) },              // slti     $rt, $rs, imm
+        { "sltiu", new InstructionMetadata("sltiu", OperationCode.SetLessThanImmediateUnsigned, StandardIPattern) },    // sltiu    $rt, $rs, imm
+
+        { "andi", new InstructionMetadata("andi", OperationCode.AndImmediate, StandardIPattern) },                      // andi     $rt, $rs, imm
+        { "ori", new InstructionMetadata("ori", OperationCode.OrImmediate, StandardIPattern) },                         // ori      $rt, $rs, imm
+        { "xori", new InstructionMetadata("xori", OperationCode.ExclusiveOrImmediate, StandardIPattern) },              // xori     $rt, $rs, imm
+
+        { "lui", new InstructionMetadata("lui", OperationCode.LoadUpperImmediate, LoadImmediatePattern) },              // lui      $rt, imm
+
+        // TODO: CoProcessing
+        
+        { "lb", new InstructionMetadata("lb", OperationCode.LoadByte, MemoryPattern) },                                 // lb       $rt, offset($rs)
+        { "lh", new InstructionMetadata("lh", OperationCode.LoadHalfWord, MemoryPattern) },                             // lh       $rt, offset($rs)
+        { "lw", new InstructionMetadata("lw", OperationCode.LoadWord, MemoryPattern) },                                 // lw       $rt, offset($rs)
+        { "lbu", new InstructionMetadata("lbu", OperationCode.LoadByteUnsigned, MemoryPattern) },                       // lbu      $rt, offset($rs)
+        { "lhu", new InstructionMetadata("lhu", OperationCode.LoadHalfWordUnsigned, MemoryPattern) },                   // lhu      $rt, offset($rs)
+                                                                                                                       
+        { "sb", new InstructionMetadata("sb", OperationCode.StoreByte, MemoryPattern) },                                // sb       $rt, offset($rs)
+        { "sh", new InstructionMetadata("sh", OperationCode.StoreHalfWord, MemoryPattern) },                            // sh       $rt, offset($rs)
+        { "sw", new InstructionMetadata("sw", OperationCode.StoreWord, MemoryPattern) },                                // sw       $rt, offset($rs)
+    };
+
+    private static readonly Dictionary<string, Register> _registerTable = new()
+    {
+        { "zero", Register.Zero },
+
+        { "at", Register.AssemblerTemporary },
+
+        { "v0", Register.ReturnValue0 },
+        { "v1", Register.ReturnValue1 },
+
+        { "a0", Register.Argument0 },
+        { "a1", Register.Argument1 },
+        { "a2", Register.Argument2 },
+        { "a3", Register.Argument3 },
+
+        { "t0", Register.Temporary0 },
+        { "t1", Register.Temporary1 },
+        { "t2", Register.Temporary2 },
+        { "t3", Register.Temporary3 },
+        { "t4", Register.Temporary4 },
+        { "t5", Register.Temporary5 },
+        { "t6", Register.Temporary6 },
+        { "t7", Register.Temporary7 },
+
+        { "s0", Register.Saved0 },
+        { "s1", Register.Saved1 },
+        { "s2", Register.Saved2 },
+        { "s3", Register.Saved3 },
+        { "s4", Register.Saved4 },
+        { "s5", Register.Saved5 },
+        { "s6", Register.Saved6 },
+        { "s7", Register.Saved7 },
+
+        { "t8", Register.Temporary8 },
+        { "t9", Register.Temporary9 },
+
+        { "k0", Register.Kernel0 },
+        { "k1", Register.Kernel1 },
+
+        { "gp", Register.GlobalPointer },
+        { "sp", Register.StackPointer },
+        { "fp", Register.FramePointer },
+
+        { "ra", Register.ReturnAddress },
+    };
+
+    /// <summary>
+    /// Attempts to get an instruction by name.
+    /// </summary>
+    /// <param name="name">The name of the instruction.</param>
+    /// <param name="metadata">The instruction metadata.</param>
+    /// <returns>Whether or not an instruction exists by that name</returns>
+    public static bool TryGetInstruction(string name, out InstructionMetadata metadata)
+        => _instructionTable.TryGetValue(name, out metadata);
+
+    /// <summary>
+    /// Attempts to get an register by name.
+    /// </summary>
+    /// <param name="name">The name of the register.</param>
+    /// <param name="register">The register enum value.</param>
+    /// <returns>Whether or not an register exists by that name</returns>
+    public static bool TryGetRegister(string name, out Register register)
+        => _registerTable.TryGetValue(name, out register);
+}
---
+++ MIPS/InstructionParser.cs
@@ -0,0 +1,284 @@
+// Adam Dernis 2023
+
+//
+//                                    Excerpt Summary
+// ----------------------------------------------------------------------------
+// 
+//      This is an excerpt of a MIPS assmebler that I wrote out of self interest.
+// The instruction parser will identify register components and immediate components.
+// The immediate components are handled by an ExpressionParser (not included) and
+// the registers are parsed in TryParseRegister, then read from a table (included).
+//
+//      The ExpressionParser also needs access to the symbol and macro table, so the
+// ObjectModuleConstructor is passed through the constructor into the ExpressionParser.
+// This is an optional argument though, to allow for tests on the InstructionParser
+// without creating an object module. The expressions simply won't contain symbols
+// or macros.
+//
+//      At this stage, it throws exceptions in place of logging errors. However, there
+// are notes through out the code dropping reminders of problems that may arise once errors
+// are implemented.
+//
+// 
+
+namespace WorkSample;
+
+/// <summary>
+/// A struct for parsing instructions.
+/// </summary>
+public struct InstructionParser
+{
+    // Definition not included in excerpt
+    private ExpressionParser _expParser;
+    
+    // Definition not included in excerpt
+    private InstructionMetadata _meta;
+
+    private OperationCode _opCode;
+    private FunctionCode _funcCode;
+    private Register _rs;
+    private Register _rt;
+    private Register _rd;
+    private byte _shift;
+    private short _immediate;
+    private uint _address;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InstructionParser"/> struct.
+    /// </summary>
+    public InstructionParser() : this(null)
+    {
+
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InstructionParser"/> struct.
+    /// </summary>
+    public InstructionParser(ObjectModuleConstructor? obj)
+    {
+        _expParser = new ExpressionParser(obj);
+        _meta = default;
+        _opCode = default;
+        _funcCode = default;
+        _rs = default;
+        _rt = default;
+        _rd = default;
+        _shift = default;
+        _immediate = default;
+        _address = default;
+    }
+
+    /// <summary>
+    /// Parses an instruction from a name and a list of arguments.
+    /// </summary>
+    /// <param name="name">The instruction name.</param>
+    /// <param name="args">The instruction arguments.</param>
+    /// <returns>An <see cref="Instruction"/>.</returns>
+    public Instruction Parse(string name, string[] args)
+    {
+        // Get instruction metadata from name
+        if (!ConstantTables.TryGetInstruction(name, out _meta))
+        {
+            // TODO: Log error
+            ThrowHelper.ThrowInvalidDataException($"Instruction named '{name}' could not be found.");
+        }
+
+        // Assert proper argument count for instruction
+        if (args.Length != _meta.ArgumentPattern.Length)
+        {
+            // TODO: Log error
+            ThrowHelper.ThrowArgumentException($"Instruction '{name}' had {args.Length} arguments instead of {_meta.ArgumentPattern.Length}.");
+        }
+
+        // Assign op code and function code
+        _opCode = _meta.OpCode;
+        _funcCode = _meta.FuncCode;
+
+        // Parse argument data according to pattern
+        var pattern = _meta.ArgumentPattern;
+        for (int i = 0; i < args.Length; i++)
+        {
+            ParseArg(args[i], pattern[i]);
+        }
+
+        // Create an instruction from its components based on the instruction type
+        var instruction = _meta.Type switch
+        {
+            InstructionType.R => Instruction.Create(_funcCode, _rs, _rt, _rd, _shift),
+            InstructionType.I => Instruction.Create(_opCode, _rs, _rt, _immediate),
+            InstructionType.J => Instruction.Create(_opCode, _address),
+            _ => ThrowHelper.ThrowInvalidDataException<Instruction>(""),
+        };
+
+        return instruction;
+    }
+
+    private void ParseArg(string arg, Argument type)
+    {
+        switch (type)
+        {
+            // Register type argument
+            case Argument.RS:
+            case Argument.RT:
+            case Argument.RD:
+                ParseRegisterArg(arg, type);
+                break;
+            // Immediate type argument
+            case Argument.Shift:
+            case Argument.Immediate:
+            case Argument.Address:
+                ParseExpressionArg(arg, type);
+                break;
+            // Address offset type argument
+            case Argument.AddressOffset:
+                ParseAddressOffsetArg(arg);
+                break;
+            default:
+                ThrowHelper.ThrowArgumentOutOfRangeException($"Argument type '{type}' is not in range.");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Parses an argument as a register and assigns it to the target component.
+    /// </summary>
+    private void ParseRegisterArg(string arg, Argument target)
+    {
+        // Get reference to selected register argument
+        ref Register reg = ref _rs;
+        switch (target)
+        {
+            case Argument.RS:
+                reg = ref _rs;
+                break;
+            case Argument.RT:
+                reg = ref _rt;
+                break;
+            case Argument.RD:
+                reg = ref _rd;
+                break;
+
+            default:
+                ThrowHelper.ThrowArgumentException($"Argument '{arg}' of type '{target}' attempted to parse as an expression.");
+                break;
+        }
+
+        if (!TryParseRegister(arg, out var register))
+        {
+            // TODO: Log error
+            // NOTE: This condition is currently not hit because any branch of
+            // TryParseRegister that would return false also throws an exception.
+            // When error messages are added the exceptions will be removed.
+        }
+
+        // Cache register as appropriate argument type
+        reg = register;
+    }
+
+    /// <summary>
+    /// Parses an argument as an expression and assigns it to the target component
+    /// </summary>
+    private void ParseExpressionArg(string arg, Argument target)
+    {
+        if (!_expParser.TryParse(arg, out var value))
+        {
+            // TODO: Log error and remove exception
+
+            ThrowHelper.ThrowArgumentException($"Argument '{arg}' of type '{target}' was not a valid expression.");
+        }
+
+        // Assign to appropriate instruction argument
+        switch (target)
+        {
+            case Argument.Shift:
+                _shift = (byte)value;
+                break;
+            case Argument.Immediate:
+                _immediate = (short)value;
+                break;
+            case Argument.Address:
+                _address = (uint)value;
+                break;
+            default:
+                ThrowHelper.ThrowArgumentOutOfRangeException($"Argument '{arg}' of type '{target}' attempted to parse as an expression.");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Parses an argument as an address offset, assigning its components to immediate and $rs.
+    /// </summary>
+    private void ParseAddressOffsetArg(string arg)
+    {
+        // Split the string into an offset and a register 
+        if (!TokenizeAddressOffset(arg, out var offsetStr, out var regStr))
+        {
+            // NOTE: This condition is currently not hit because any branch of
+            // TokenizeAddressOffset that would return false also throws an exception.
+            // When error messages are added the exceptions will be removed.
+            return;
+        }
+        
+        // NOTE: Be careful about forwards to other parse functions when implementing 
+        // error logging. $rs and immediate errors might be inappropriately logged for
+        // address offset arguments.
+
+
+        // Parse offset component into immediate
+        ParseExpressionArg(offsetStr, Argument.Immediate);
+
+        // Parse register component into $rs
+        ParseRegisterArg(regStr, Argument.RS);
+    }
+
+    private static bool TryParseRegister(string name, out Register register)
+    {
+        // Trim whitespace from register string
+        name = name.Trim();
+
+        // Check that argument is register argument
+        if (name[0] != '$')
+        {
+            // TODO: Log error and remove exception.
+
+            ThrowHelper.ThrowArgumentException($"Expected register argument. Found '{name}'");
+            register = Register.Zero;
+            return false;
+        }
+
+        // Get register from table
+        if (!ConstantTables.TryGetRegister(name[1..], out register))
+        {
+            // TODO: Log error and remove exception.
+
+            ThrowHelper.ThrowInvalidDataException($"{name} is not a valid register.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool TokenizeAddressOffset(string arg, out string offset, out string register)
+    {
+        // Find parenthesis start and end
+        // Parenthesis wrap the register
+        int regStart = arg.IndexOf('(');
+        int regEnd = arg.IndexOf(')');
+
+        // Either end of the parenthesis were not found
+        if (regStart == -1 || regEnd == -1)
+        {
+            offset = string.Empty;
+            register = string.Empty;
+
+            ThrowHelper.ThrowArgumentException($"Argument '{arg}' is not a valid address offset.");
+            return false;
+        }
+
+        // Split argument into offset and register components
+        offset = arg[..regStart];
+        register = arg[(regStart + 1)..regEnd];
+
+        return true;
+    }
+}
